007b02047d1a3a932a12fc2ae5fb6007b4c6baa4
wzhang28 <wzhang28@studentmail.nait.ca>
2016-11-15 15:46:01 -0700

    11/15/2016

---
+++ eRestaurant Demo/eRestaurant/BLL/eRestaurantReportController.cs
@@ -0,0 +1,35 @@
+﻿using eRestaurant.DAL;
+using eRestaurant.Entities.Pocos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.BLL
+{
+    [DataObject]
+    class eRestaurantReportController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<CategoryMenuItem> GetReportCategoryMenuItems()
+        {
+            using (var context = new RestaurantContext())
+            {
+                var results = from row in context.Items
+                //                \row/ is an instance of the Item entity
+                              select new CategoryMenuItem()
+                              {
+                                  CategoryDescription = row.MenuCategory.Description,
+                                  ItmDescription = row.Description,
+                                  Price = row.CurrentPrice,
+                                  Calories = row.Calories,
+                                  Comment = row.Comment
+                              };
+
+                return results.ToList();
+            }
+        }
+    }
+}
---
+++ eRestaurant Demo/eRestaurant/Entities/Pocos/CategoryMenuItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.Pocos
+{
+    public class CategoryMenuItem
+    {
+        public string CategoryDescription { get; set; }
+        public string ItmDescription { get; set; }
+        public decimal Price { get; set; }
+        public int? Calories { get; set; }
+        public string Comment { get; set; }
+    }
+}
